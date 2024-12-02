3ffb9ea4ab9b319e7e5c4936b4c27937248e7796
andresnico44 <ramiroandresnc@ufps.edu.co>
2024-12-01 23:28:46 -0500

    Base examen final

---
+++ src/main/java/co/edu/ufps/ExamenFinalWeb1152290Application.java
@@ -0,0 +1,13 @@
+package co.edu.ufps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalWeb1152290Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFinalWeb1152290Application.class, args);
+	}
+
+}
